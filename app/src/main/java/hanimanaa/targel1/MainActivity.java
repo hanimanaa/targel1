07df10843d0904868f3a7e1476e82404d2d2e24f
hani_pc <hanimanaa@gmail.com>
2019-11-26 20:53:11 +0200

    fh

--- app/src/main/java/hanimanaa/targel1/MainActivity.java
+++ app/src/main/java/hanimanaa/targel1/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     int x;
     int y;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
